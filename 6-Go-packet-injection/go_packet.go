c1990d40858cf9515ea306be1c01fc3f4ec51833
tyagian <tyagi.an@husky.neu.edu>
2019-05-16 00:11:55 -0400

    packet capture interface

--- 6-Go-packet-injection/go_packet.go
+++ 6-Go-packet-injection/go_packet.go
@@ -1,18 +1,27 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"log"
+
 	"github.com/google/gopacket/pcap"
 )
+
 func main() {
 	// find all Device
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
 		log.Fatal(err)
 	}
-	for i in devces {
-		fmt.Println(i)
+	for _, device := range devices {
+		fmt.Println("Name:", device.Name)
+		fmt.Println("Description", device.Description)
+		fmt.Println("Devices address", device.Description)
+
+		for _, address := range device.Addresses {
+			fmt.Println("- IP address", address.IP)
+			fmt.Println("- Subnet mask:", address.Netmask)
+		}
 	}
 
 }
