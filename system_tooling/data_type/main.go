ca5ae371e7e729c7e3d04e080b181cad5e1a620c
tyagian <tyagi.an@husky.neu.edu>
2020-11-15 19:28:43 -0500

    8.1 creating cli

--- system_tooling/data_type/main.go
+++ system_tooling/data_type/main.go
@@ -1,12 +1,79 @@
 package main
 
 import (
+	"GO_Mini_Projects/system_tooling/data_type/message"
+	"bufio"
+	"flag"
 	"fmt"
-	"system_tooling/data_type/message"
+	"os"
+	"strings"
 )
 
 func main() {
+	// Define Flags
+	var name string
+	var greeting string
+	var prompt bool
+	var preview bool
 
-	message := message.greeting("Keith", "Hello")
-	fmt.Println(message)
+	// Parse Flags
+	flag.StringVar(&name, "name", "", "name to use within the messgae")
+	flag.StringVar(&greeting, "greeting", "", "phrase to use within the message")
+	flag.BoolVar(&prompt, "prompt", false, "use prompt to input name")
+	flag.BoolVar(&preview, "preview", false, "user preview to output message")
+	flag.Parse()
+	// Show usage if flags are invalid
+	if prompt == false && (name == "" || greeting == "") {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	//Optionally print flags & exit based on DEBUG environment variable
+	if os.Getenv("DEBUG") != "" {
+		fmt.Println("Name:", name)
+		fmt.Println("Greeting:", greeting)
+		fmt.Println("Prompt:", prompt)
+		fmt.Println("Preview:", preview)
+		os.Exit(0)
+	}
+
+	//conditionally read from stdin
+	if prompt {
+		name, greeting = renderPrompt()
+	}
+
+	// Generate message
+	message := message.Greeting(name, greeting)
+
+	// Either preview the message or write to the file
+	if preview {
+		fmt.Println(message)
+	} else {
+		// Write content
+		f, err := os.OpenFile("/etc/motd", os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Error: Unable to open /etc/motd")
+			os.Exit(1)
+		}
+		defer f.Close()
+		_, err = f.Write([]byte(message))
+		//fmt.Println(message)
+		if err != nil {
+			fmt.Println("Error: Failed to write to /etc/motd")
+			os.Exit(1)
+		}
+
+	}
+}
+
+func renderPrompt() (name, greeting string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Your Greeting: ")
+	greeting, _ = reader.ReadString('\n')
+	greeting = strings.TrimSpace(greeting)
+
+	fmt.Print("Your Name: ")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	return
 }
--- system_tooling/data_type/message/greeting.go
+++ system_tooling/data_type/message/greeting.go
@@ -2,7 +2,7 @@ package message
 
 import "fmt"
 
-func greeting(name, message string) (salutation string) {
-	salutation = fmt.Sprintf("%s", "%s", message, name)
+func Greeting(name, message string) (salutation string) {
+	salutation = fmt.Sprintf("%s, %s", message, name)
 	return
 }
