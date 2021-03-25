5c88c61e49c16cf4ffb4c0fc9e87a5758002625a
tyagian <tyagi.an@husky.neu.edu>
2021-03-24 22:39:00 -0400

    add, divide func

--- website_in_go/3-building-basic-webapp/hello_world_webapp/main.go
+++ website_in_go/3-building-basic-webapp/hello_world_webapp/main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is Home page")
+}
+
+// About page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about page and 2+2 is %d", sum))
+}
+
+// addValues add two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 	// https://golang.org/pkg/net/http/
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Homepage")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	//log.Fatal(http.ListenAndServe("portNumber", nil))
+	_ = http.ListenAndServe(portNumber, nil)
 }
