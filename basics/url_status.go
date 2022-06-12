f996692d7a3c67deb12e7f7215f17e779f2fa989
Anuj <tyagi.an@northeastern.edu>
2022-06-11 22:24:58 -0400

    get url status

---
+++ basics/url_status.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+	//var args []string
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url> \n")
+		os.Exit(1)
+	}
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+	//var response *http.Response
+
+	response, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, http.StatusText(response.StatusCode)) //body)
+	_ = body
+}
