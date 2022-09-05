28bd4a7a253a257bdf1dd566c8bc7875cc5e9673
tyagian <tyagi.an@husky.neu.edu>
2022-09-05 18:56:51 -0400

    handleUrl

---
+++ 0_basics/17_handle_url/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghnj456"
+
+func main() {
+
+	fmt.Println("Welcome to handling urls in Golang")
+	fmt.Println(myurl)
+
+	// parse url
+	result, _ := url.Parse(myurl)
+
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Path)
+	// Port is a method not property
+	fmt.Println(result.Port())
+	fmt.Println(result.RawQuery)
+
+	qparams := result.Query()
+	// url.Values
+	fmt.Printf("The type of query params are: %T \n", qparams)
+	fmt.Println(qparams["coursename"])
+
+	for _, val := range qparams {
+		fmt.Println("Params is:", val)
+	}
+	partsOfUrl := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "user=hitesh",
+	}
+
+	anotherURL := partsOfUrl.String()
+	fmt.Println(anotherURL)
+}
