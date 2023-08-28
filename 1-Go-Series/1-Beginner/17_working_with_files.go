f8063ae3029a92dc7096a4a82867c26bc3249f2c
tyagian <tyagi.an@husky.neu.edu>
2023-08-28 17:53:15 -0400

    url handling

--- 1-Go-Series/1-Beginner/17_working_with_files.go
+++ 1-Go-Series/1-Beginner/17_working_with_files.go
@@ -3,23 +3,37 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	fmt.Println("file example")
 	content := "This needs to go in a file - example.com"
-
 	file, err := os.Create("./myfile.txt")
-
-	if err != nil {
-		panic(err)
-	}
-
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilErr(err)
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 	fmt.Println("length is", length)
 	defer file.Close()
+	readFile("./myfile.txt")
+}
+
+func readFile(filname string) {
+	databyte, err := ioutil.ReadFile(filname)
+
+	checkNilErr(err)
+
+	// fmt.Println("Text data inside the file is \n", databyte)
+	// databyte by default will throw binary
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
